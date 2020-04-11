5b62e3972c1855e63e919e219b71fcf008e36bbe
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-04-11 11:56:42 -0300

    update(sampleScene): tests involving the new so architecture

---
+++ Assets/Sandbox/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using SOArchitecture;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private GameEventFloat gameEventFloat;
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(2f);
+        gameEventFloat.Raise(3.4f);
+    }
+
+    public void ShowValue(float value)
+    {
+        print(value);
+    }
+}
