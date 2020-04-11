abb4be385db1e13424cdff53b1267c40e31f912f
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-04-11 12:31:48 -0300

    update(GameEventListenerBase.cs): void option

--- Assets/SO-Architecture/GameEvents/GameEventListenerBase.cs
+++ Assets/SO-Architecture/GameEvents/GameEventListenerBase.cs
@@ -3,7 +3,30 @@ using UnityEngine.Events;
 
 namespace SOArchitecture
 {
-    public class GameEventListenerBase<TValue, TGameEvent, TUnityEvent> : MonoBehaviour, IGameEventListener<TValue>
+    public abstract class GameEventListenerBase<TGameEvent, TUnityEvent> : MonoBehaviour, IGameEventListener
+        where TGameEvent : IGameEvent
+        where TUnityEvent : UnityEvent
+    {
+        [SerializeField] private TGameEvent gameEvent;
+        [SerializeField] private TUnityEvent response;
+
+        public void OnEnable()
+        {
+            gameEvent.Register(this);
+        }
+
+        public void OnDisable()
+        {
+            gameEvent.Unregister(this);
+        }
+
+        public void OnEventRaised()
+        {
+            response.Invoke();
+        }
+    }
+    
+    public abstract class GameEventListenerBase<TValue, TGameEvent, TUnityEvent> : MonoBehaviour, IGameEventListener<TValue>
         where TGameEvent : IGameEvent<TValue>
         where TUnityEvent : UnityEvent<TValue>
     {
