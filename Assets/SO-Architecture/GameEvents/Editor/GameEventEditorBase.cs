e79f0f170b0beda24492b91cc7fd2d35d802ae8a
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-04-19 13:35:58 -0300

    update(void game event editor): now the void game event has a new editor for simulate the event in the game and creating interfaces

--- Assets/SO-Architecture/GameEvents/Editor/GameEventEditorBase.cs
+++ Assets/SO-Architecture/GameEvents/Editor/GameEventEditorBase.cs
@@ -4,6 +4,68 @@ using UnityEngine;
 
 namespace SOArchitecture
 {
+    public abstract class GameEventEditorBase<TGameEvent> : Editor
+        where TGameEvent : GameEventBase
+    {
+        protected string NewInterfaceFilePath;
+        private TGameEvent _gameEvent;
+
+        private void OnEnable()
+        {
+            _gameEvent = target as TGameEvent;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUI.BeginDisabledGroup(Application.isPlaying);
+
+            EditorGUILayout.HelpBox("Do not create game events with white spaces", MessageType.Warning);
+
+            var buttonStyle = new GUIStyle(GUI.skin.button)
+            {
+                alignment = TextAnchor.MiddleCenter,
+                fixedWidth = 200f,
+                margin = new RectOffset(0, 0, 10, 20),
+            };
+
+            if (GUILayout.Button("Create interface", buttonStyle))
+            {
+                CreateInterface(_gameEvent.name);
+            }
+
+            EditorGUI.EndDisabledGroup();
+            
+            EditorGUI.BeginDisabledGroup(!Application.isPlaying);
+            
+            GUILayout.Label("Press the Raise button to simulate the event in the game");
+            if (GUILayout.Button("Raise"))
+            {
+                _gameEvent.Raise();
+            }
+            
+            EditorGUI.EndDisabledGroup();
+        }
+        
+        protected virtual void CreateInterface(string name)
+        {
+            var fullPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+            var fileName = string.Concat("I", name, ".cs");
+
+            var directoryPath = string.Concat(fullPath.Substring(0, fullPath.Length - name.Length - 6), "Interfaces/");
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            NewInterfaceFilePath = string.Concat(directoryPath, fileName);
+            
+            if (File.Exists(NewInterfaceFilePath))
+            {
+                File.Delete(NewInterfaceFilePath);
+            }
+        }
+    }
+    
     public abstract class GameEventEditorBase<TValue, TGameEvent> : Editor
         where TGameEvent : GameEventBase<TValue>
     {
@@ -34,11 +96,6 @@ namespace SOArchitecture
             }
             EditorGUI.EndDisabledGroup();
             
-            var rightAlignment = new GUIStyle(GUI.skin.label)
-            {
-                alignment = TextAnchor.LowerRight
-            };
-
             var boldText = new GUIStyle(GUI.skin.label)
             {
                 fontStyle = FontStyle.Bold
--- Assets/SO-Architecture/Types/Void/Editor/VoidGameEventEditor.cs
+++ Assets/SO-Architecture/Types/Void/Editor/VoidGameEventEditor.cs
@@ -1,6 +1,21 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEditor;
+using SOArchitecture;
 
-public class VoidGameEventEditor : MonoBehaviour
+[CustomEditor(typeof(VoidGameEvent))]
+public class VoidGameEventEditor : GameEventEditorBase<VoidGameEvent>
 {
-    
+    protected override void CreateInterface(string name)
+    {
+        base.CreateInterface(name);
+        using (var outfile = new StreamWriter(NewInterfaceFilePath))
+        {
+            outfile.WriteLine(string.Concat("public interface I", name));
+            outfile.WriteLine("{");
+            outfile.WriteLine(string.Concat("    void ", name, "();"));
+            outfile.WriteLine("}");
+        }
+
+        AssetDatabase.Refresh();
+    }
 }
