5d85b65a9785a8cf43fec8826389440399296dd7
CaiqueTorres <caiquetorres.bastos2807@gmail.com>
2020-06-11 18:07:38 -0300

    style(collection): removed white spaces

--- Assets/SO-Architecture/Collections/Scripts/Collection.cs
+++ Assets/SO-Architecture/Collections/Scripts/Collection.cs
@@ -14,33 +14,33 @@ namespace SOArchitecture
             get => items[index];
             set => items[index] = value;
         }
-        public int Count => items.Count;
         
+        public int Count => items.Count;
         public bool IsReadOnly { get; }
-
+        
         public void Add(TValue item)
         {
             items.Add(item);
             onChangedCollection.Raise();
         }
-
+        
         public void Clear()
         {
             items.Clear();
             onChangedCollection.Raise();
         }
-
+        
         public bool Contains(TValue item) => items.Contains(item);
-
+        
         public void CopyTo(TValue[] array, int arrayIndex) => items.CopyTo(array, arrayIndex);
-
+        
         public bool Remove(TValue item)
         {
             var removed = items.Remove(item);
             onChangedCollection.Raise();
             return removed;
         }
-
+        
         public int IndexOf(TValue item) => items.IndexOf(item);
         
         public void Insert(int index, TValue item)
@@ -48,17 +48,18 @@ namespace SOArchitecture
             items.Insert(index, item);
             onChangedCollection.Raise();
         }
-
+        
         public void RemoveAt(int index)
         {
             items.RemoveAt(index);
             onChangedCollection.Raise();
         }
-
+        
         public IEnumerator<TValue> GetEnumerator() => items.GetEnumerator();
-
+        
         IEnumerator IEnumerable.GetEnumerator() => items.GetEnumerator();
-
+        
         public List<TValue> GetList() => items;
+        
     }
 }
