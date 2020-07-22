e7f6e5046171938989752cf6ae2537e4c9f51d4e
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-07-22 11:25:41 -0300

    feat(comments): add comments in the SOArchitectureHelpers

--- Assets/SOArchitecture/Helpers/Editor/SOArchitectureEditorHelpers.cs
+++ Assets/SOArchitecture/Helpers/Editor/SOArchitectureEditorHelpers.cs
@@ -6,6 +6,11 @@ namespace SOArchitecture
 {
     public static class SOArchitectureEditorHelpers
     {
+        /// <summary>
+        /// Static method that draw in the inspector the description of the ScriptableObject
+        /// </summary>
+        /// <param name="descriptionProperty">Parameter that indicates the property that will be drawn</param>
+        /// <param name="showing">Parameter that indicates if the property is being drawing in the inspector</param>
         public static void DrawDescription(SerializedProperty descriptionProperty, ref bool showing)
         {
             showing = EditorGUILayout.Foldout(showing, "Description");
@@ -13,6 +18,10 @@ namespace SOArchitecture
                 EditorGUILayout.PropertyField(descriptionProperty, GUIContent.none);
         }
 
+        /// <summary>
+        /// Static method that create a .cs file with an interface
+        /// </summary>
+        /// <param name="name">Parameter that indicates the Game Event name</param>
         public static void CreateInterface(string name)
         {
             var fullPath = AssetDatabase.GetAssetPath(Selection.activeObject);
@@ -38,6 +47,11 @@ namespace SOArchitecture
             AssetDatabase.Refresh();
         }
 
+        /// <summary>
+        /// Static method that create a .cs file with an interface
+        /// </summary>
+        /// <param name="name">Parameter that indicates the Game Event name</param>
+        /// <typeparam name="TValue">Parameter that indicates the type of the Game Event</typeparam>
         public static void CreateInterface<TValue>(string name)
         {
             var fullPath = AssetDatabase.GetAssetPath(Selection.activeObject);
