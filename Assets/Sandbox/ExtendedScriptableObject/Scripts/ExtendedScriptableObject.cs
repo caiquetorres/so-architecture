903ee3aa8aace276e1efc39c3920e3917c079d28
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-07-23 20:07:11 -0300

    feat(extended scriptable object): create the main script

---
+++ Assets/Sandbox/ExtendedScriptableObject/Scripts/ExtendedScriptableObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SOArchitecture
+{
+    public class ExtendedScriptableObject : PropertyAttribute { }
+
+    [CustomPropertyDrawer(typeof(ExtendedScriptableObject))]
+    public class ExtendedScriptableObjectDrawer : PropertyDrawer
+    {
+        
+    }
+}
