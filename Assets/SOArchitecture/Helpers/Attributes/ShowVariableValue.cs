6658bfef058fce0f9dbf09ba45a1032787033495
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-07-27 12:53:06 -0300

    feat(extended scriptable object): add a new editor that allows to see the scriptable object field values in another inspectors

--- Assets/SOArchitecture/Helpers/Attributes/ShowVariableValue.cs
+++ Assets/SOArchitecture/Helpers/Attributes/ShowVariableValue.cs
@@ -16,45 +16,49 @@ namespace SOArchitecture
     [CustomPropertyDrawer(typeof(ShowVariableValue))]
     public class ShowVariableDrawer : PropertyDrawer
     {
-        private float _lastWidth;
-        private const float Padding = 4f;
+        private readonly float _padding = EditorGUIUtility.standardVerticalSpacing;
+        private const string ValuePropertyName = "value";
         
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            _lastWidth = GUILayoutUtility.GetLastRect().width;
-            
-            EditorGUI.BeginProperty(position, label, property);
+            var lastWidth = position.width;
+            var objectReferenceValue = property.objectReferenceValue; 
             var canEditVariable = attribute as ShowVariableValue;
-            
-            var rectVariable = new Rect()
+
+            var rectVariable = new Rect
             {
                 x = position.x,
                 y = position.y,
-                width = _lastWidth * 0.8f,
+                width = lastWidth * 0.8f,
                 height = EditorGUIUtility.singleLineHeight,
             };
             
-            var rectValue = new Rect()
+            var rectValue = new Rect
             {
-                x = position.x + _lastWidth * 0.8f + Padding,
+                x = position.x + lastWidth * 0.8f + _padding,
                 y = position.y,
-                width = _lastWidth * 0.2f - Padding,
+                width = position.width * 0.2f - _padding,
                 height = EditorGUIUtility.singleLineHeight,
             };
             
-            EditorGUI.PropertyField(rectVariable, property, label);
-
-            if (property.objectReferenceValue != null)
+            EditorGUI.BeginProperty(position, label, property);
             {
-                var serializedObject = new SerializedObject(property.objectReferenceValue);
-                EditorGUI.BeginDisabledGroup(!canEditVariable.CanEdit);
-                
-                    EditorGUI.PropertyField(rectValue, serializedObject.FindProperty("value"), GUIContent.none);
-                    serializedObject.ApplyModifiedProperties();
-                    
-                EditorGUI.EndDisabledGroup();
+                EditorGUI.PropertyField(rectVariable, property, label);
+
+                if (objectReferenceValue != null)
+                {
+                    var serializedObject = new SerializedObject(objectReferenceValue);
+                    EditorGUI.BeginDisabledGroup(canEditVariable != null && !canEditVariable.CanEdit);
+                    {
+                        EditorGUI.PropertyField(
+                            rectValue, serializedObject.FindProperty(ValuePropertyName), GUIContent.none
+                        );
+                        
+                        serializedObject.ApplyModifiedProperties();
+                    }
+                    EditorGUI.EndDisabledGroup();
+                }
             }
-            
             EditorGUI.EndProperty();
         }
     }
