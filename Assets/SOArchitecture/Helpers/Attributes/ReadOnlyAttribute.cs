c4a3556cf00855b654171af9d9ab79f41130ff32
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-07-27 12:02:06 -0300

    feat(read only attribute): create new editor that works as read only

---
+++ Assets/SOArchitecture/Helpers/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SOArchitecture
+{
+    public class ReadOnly : PropertyAttribute { }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(ReadOnly))]
+    public class ReadOnlyPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginDisabledGroup(true);
+            {
+                EditorGUI.PropertyField(position, property, label);
+            }
+            EditorGUI.EndDisabledGroup();
+        }
+    }
+#endif
+}
--- Assets/Sandbox/Test/ScriptableObjectTest.cs
+++ Assets/Sandbox/Test/ScriptableObjectTest.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 [CreateAssetMenu] public class ScriptableObjectTest : ScriptableObject
 {
     [SerializeField] private int age;
-    [SerializeField] private new string name;
+    [ReadOnly, SerializeField] private new string name;
     [ShowVariableValue, SerializeField] private DirectionVariable directionVariable;
 }
